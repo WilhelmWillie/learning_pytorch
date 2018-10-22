0f241de021dd809bce98a94461292af6873f6be3
Wilhelm Willie <wilhelmwillie@gmail.com>
2018-10-22 12:50:33 -0700

    TO-DO: Build a basic text classifier

---
+++ basic_nlp/basic_nlp.py
@@ -0,0 +1,8 @@
+'''
+Goal: build a basic text classifier that can differentiate the subject
+matter of different texts. For my first attempt, I want to build a simple
+classifier that tags text by their industry
+
+Ex: tag a paragraph as being one related to the tech industry, another
+as being related to the automobile industry, etc.
+'''
